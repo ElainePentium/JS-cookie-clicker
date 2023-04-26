efd922193510250ab0f166579a24b1662b90653f
Ferreira Josué <ferreira.josu@gmail.com>
2023-04-26 12:08:36 +0200

    js modify number and prices of objects in HTML

--- army.js
+++ army.js
@@ -1,8 +1,21 @@
-toto = new Click();
-toto.buy();
+// Instance Objects
+clickFarming = new Click();
+grandMaFarming = new GrandMa();
+farmFarming = new Farm();
+
 
 // document.querySelector('#army-click .stats').append("JS must add here : numberClick, inProdCookiesClick, inProdAllCookiesClick, productedCookiesByClick");
-document.querySelector('#army-grandma .stats').append("JS must add here : numberGMa, inProdCookiesGMa, inProdAllCookiesGMa, productedCookiesByGMa");
-document.querySelector('#army-farm .stats').append("JS must add here : numberFarm, inProdCookiesFarm, inProdAllCookiesFarm, productedCookiesByFarm");
+// document.querySelector('#army-grandma .stats').append("JS must add here : numberGMa, inProdCookiesGMa, inProdAllCookiesGMa, productedCookiesByGMa");
+// document.querySelector('#army-farm .stats').append("JS must add here : numberFarm, inProdCookiesFarm, inProdAllCookiesFarm, productedCookiesByFarm");
 
-document.querySelector('#army-click .stats').append(`NUMBER : ${toto.number} PRICE : ${toto.price}`);
+document.querySelector('#army-click .stats').append(`NUMBER : ${clickFarming.number}`);
+document.querySelector('#army-click .stats').insertAdjacentHTML('beforeend','<br>');
+document.querySelector('#army-click .stats').append(`PRICE : ${clickFarming.price}`);
+
+document.querySelector('#army-grandma .stats').append(`NUMBER : ${grandMaFarming.number}`);
+document.querySelector('#army-grandma .stats').insertAdjacentHTML('beforeend','<br>');
+document.querySelector('#army-grandma .stats').append(`PRICE : ${grandMaFarming.price}`);
+
+document.querySelector('#army-farm .stats').append(`NUMBER : ${farmFarming.number}`);
+document.querySelector('#army-farm .stats').insertAdjacentHTML('beforeend','<br>');
+document.querySelector('#army-farm .stats').append(`PRICE : ${farmFarming.price}`);
