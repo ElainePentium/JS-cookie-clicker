b077a296d5bda06dadce1c4d1da92a2f1d31385a
Ferreira Josué <ferreira.josu@gmail.com>
2023-04-26 11:22:26 +0200

    modified diagram_class and created grower.js and class grower

--- army.js
+++ army.js
@@ -1,1 +1,3 @@
-document.querySelector('#army-click .stats').append("Hello !!");
+document.querySelector('#army-click .stats').append("JS must add here : numberClick, inProdCookiesClick, inProdAllCookiesClick, productedCookiesByClick");
+document.querySelector('#army-grandma .stats').append("JS must add here : numberGMa, inProdCookiesGMa, inProdAllCookiesGMa, productedCookiesByGMa");
+document.querySelector('#army-farm .stats').append("JS must add here : numberFarm, inProdCookiesFarm, inProdAllCookiesFarm, productedCookiesByFarm");
