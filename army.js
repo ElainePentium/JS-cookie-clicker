f58d5ff1ab4a2b716e42c2514762f3428e076d56
Ferreira Josué <ferreira.josu@gmail.com>
2023-04-26 15:10:05 +0200

    first store prototype*

--- army.js
+++ army.js
@@ -3,12 +3,6 @@ clickFarming = new Click();
 grandMaFarming = new GrandMa();
 farmFarming = new Farm();
 
-
-// document.querySelector('#army-click .stats').append("JS must add here : numberClick, inProdCookiesClick, inProdAllCookiesClick, productedCookiesByClick");
-// document.querySelector('#army-grandma .stats').append("JS must add here : numberGMa, inProdCookiesGMa, inProdAllCookiesGMa, productedCookiesByGMa");
-// document.querySelector('#army-farm .stats').append("JS must add here : numberFarm, inProdCookiesFarm, inProdAllCookiesFarm, productedCookiesByFarm");
-// setInterval(function() { for(let i=0; i<10000; i++){}},100);
-
 setInterval(function() {
     document.querySelector('#army-click .stats').innerHTML= "";
     document.querySelector('#ClickNumber').innerHTML= "";
@@ -43,22 +37,15 @@ setInterval(function() {
     clickFarming.produce();
     grandMaFarming.produce();
     farmFarming.produce();
+
+    // A déporter dans un store.js
+    // Affiche le prix des objets dans le store
+    document.querySelector('#store-click .stats').innerHTML= "";
+    document.querySelector('#store-click .stats').append(`Costs ${clickFarming.price.toFixed(2)}`);
+    document.querySelector('#store-grandma .stats').innerHTML= "";
+    document.querySelector('#store-grandma .stats').append(`Costs ${grandMaFarming.price.toFixed(2)}`);
+    document.querySelector('#store-farm .stats').innerHTML= "";
+    document.querySelector('#store-farm .stats').append(`Costs ${farmFarming.price.toFixed(2)}`);
+
+
 },1000);
-// document.querySelector('#army-click .stats').innerHTML= "";
-// document.querySelector('#army-click .stats').append(`Number : ${clickFarming.number}`);
-// document.querySelector('#army-click .stats').insertAdjacentHTML('beforeend','<br>');
-// document.querySelector('#army-click .stats').append(`Each click produces ${clickFarming.inProdCookies} per second`);
-// document.querySelector('#army-click .stats').insertAdjacentHTML('beforeend','<br>');
-// document.querySelector('#army-click .stats').append(`${clickFarming.number} clicks produce ${clickFarming.inAllProdCookies} per second`);
-// document.querySelector('#army-click .stats').insertAdjacentHTML('beforeend','<br>');
-// document.querySelector('#army-click .stats').append(`${clickFarming.productedCookies} clicked so far`);
-
-// document.querySelector('#army-grandma .stats').append(`NUMBER : ${grandMaFarming.number}`);
-// document.querySelector('#army-grandma .stats').insertAdjacentHTML('beforeend','<br>');
-// document.querySelector('#army-grandma .stats').append(`PRICE : ${grandMaFarming.price}`);
-
-// document.querySelector('#army-farm .stats').append(`NUMBER : ${farmFarming.number}`);
-// document.querySelector('#army-farm .stats').insertAdjacentHTML('beforeend','<br>');
-// document.querySelector('#army-farm .stats').append(`PRICE : ${farmFarming.price}`);
-
-
