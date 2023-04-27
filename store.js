d54dfd97c6de4766fcaf8cdb53b2517f8537ad77
Ferreira Josué <ferreira.josu@gmail.com>
2023-04-27 14:54:44 +0200

    store tab buildings and upgrades7

--- store.js
+++ store.js
@@ -3,6 +3,20 @@ document.querySelector('#store-cursor .stats-store').insertAdjacentHTML('beforee
 document.querySelector('#store-grandma .stats-store').insertAdjacentHTML('beforeend',`Costs ${grandMaFarming.price.toFixed(2)} <i class="fa-solid fa-cookie"></i>`);
 document.querySelector('#store-farm .stats-store').insertAdjacentHTML('beforeend',`Costs ${farmFarming.price.toFixed(2)} <i class="fa-solid fa-cookie"></i>`);
 
+// Tab content viewer
+document.getElementById('buildings').addEventListener('click',(event) =>{
+    document.getElementById('buildings-tab').classList.add('is-enable');
+    document.getElementById('upgrades-tab').classList.remove('is-enable');
+    document.getElementById('buildings').classList.add('is-enable');
+    document.getElementById('upgrades').classList.remove('is-enable');
+})
+document.getElementById('upgrades').addEventListener('click',(event) =>{
+    document.getElementById('buildings-tab').classList.remove('is-enable');
+    document.getElementById('upgrades-tab').classList.add('is-enable');
+    document.getElementById('buildings').classList.remove('is-enable');
+    document.getElementById('upgrades').classList.add('is-enable');
+})
+
 // Events on buy in store
 document.getElementById('store-cursor').addEventListener('click',(event) => {
     cursorFarming.buy();
